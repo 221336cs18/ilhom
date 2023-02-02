5419efd3511cae21971e0becd696fa6c77a01719
221336cs18 <113594405+221336cs18@users.noreply.github.com>
2023-02-02 08:38:02 +0500

    Add files via upload

---
+++ githup_work.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+   /* int n=3;
+    char a;
+    cin>>a;
+    cout<<int(a);
+*/
+    /*
+    //ABC
+    for(char i='A';i<='Z';i++){
+            cout<<i<<" ";
+
+    }*/
+    /*
+    // abc
+    for(char i='a';i<='z';i++){
+        cout<<i<<" "<<int(i)<<endl;
+    }
+    */
+    /*
+
+    for(char i='1';i<='9';i++){
+        cout<<i<<" "<<int(i)<<endl;
+    }
+    */
+    /*
+    int n=3;
+    char a[100];
+    cin>>a;
+    cout<<a;
+    */
+
+    return 0;
+}
